b025729fd1fd451d26158e13566ee8bdc3c2772e
Serdar Ashirov <webapis@users.noreply.github.com>
2024-07-01 10:33:02 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import { PuppeteerCrawler } from 'crawlee';
 import { router } from './routes.js';
 import preNavigationHooks from './crawler-helper/preNavigationHooks.js';
 const site = process.env.site
+const local = process.env.local
+const windowsPath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+const linuxPath = "/usr/bin/google-chrome"
 const siteVar = await import(`./sites/${site}.js`)
 debugger
 const urls = siteVar.urls
 const crawler = new PuppeteerCrawler({
-  launchContext: { launchOptions: { executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" } },
+  launchContext: { launchOptions: { executablePath: local ? windowsPath : linuxPath } },
   requestHandler: router,
   maxConcurrency: 1,
   preNavigationHooks
