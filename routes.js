9b15934605fdfcd9063584f9dc5b9bf5d886766f
Serdar Ashirov <webapis@users.noreply.github.com>
2024-07-02 19:23:26 +0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -35,7 +35,7 @@ router.addHandler('oyuncular', async ({ request, page, log, pushData, enqueueLin
   debugger
   const handler = siteVar.oyuncular
   const data = await handler({ page, enqueueLinks, request, log, addRequests })
-
+console.log('data',data.length)
   await productsDataset.pushData(data);
   debugger
 
