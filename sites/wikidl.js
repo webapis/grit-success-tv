2f29b7a36e1655c95d23996feba35507359cf79c
Serdar Ashirov <webapis@users.noreply.github.com>
2024-07-07 12:31:49 +0500

    Update wikidl.js

--- sites/wikidl.js
+++ sites/wikidl.js
@@ -5,16 +5,15 @@ export default async function dizi({ page, enqueueLinks, request, log, addReques
     const data = await page.evaluate(() => {
         const collection = Array.from(document.querySelectorAll('[cellpadding="4"] tbody tr')).map(m => m.querySelector('td a[title]')).filter(f => f).filter(f => !f.classList.contains('new')).map(m => {
             return {
-                detailHref: m.href,
-                title: m.getAttribute('title').replace(' (dizi)', '').trim().toLocaleLowerCase(),
-                imgOrientation:"portrait",
-                imqQuatity:2
+                WIKILINK: m.href,
+                TVSERIES_TITLE: m.getAttribute('title').replace(' (dizi)', '').trim().toLocaleLowerCase(),
+
             }
         })
         return collection
     })
     for (let d of data) {
-        await addRequests([{ url: d.detailHref, label: 'oyuncular', userData: { dizi: d, initUrl: d.detailHref } }])
+        await addRequests([{ url: d.WIKILINK, label: 'oyuncular', userData: { dizi: d } }])
     }
     debugger
     return data
@@ -34,16 +33,95 @@ export async function oyuncular({ page, enqueueLinks, request, log, addRequests 
                 title: m?.querySelector('th').innerText,
                 value: m.querySelector('td').innerText.replaceAll('\n', ', ')
             }
-        })
+        }).reduce((prev, curr, i) => {
+            console.log(curr)
+            if (curr.title.includes('Format')) {
+                return { ...prev, FORMAT: curr.value }
+            } else if (curr.title.includes('Tür')) {
+                return { ...prev, GENRES: curr.value }
+            }
+            else if (curr.title.includes('Proje tasarımcısı')) {
+                return { ...prev, PROJE_TASARIMCI: curr.value }
+            }
+            else if (curr.title.includes('Senarist')) {
+                return { ...prev, SENARIST: curr.value }
+            }
+            else if (curr.title.includes('Öykü')) {
+                return { ...prev, OYKU: curr.value }
+            }
+            else if (curr.title.includes('Yönetmen')) {
+                return { ...prev, YONETMEN: curr.value }
+            }
+            else if (curr.title.includes('Başrol')) {
+                return { ...prev, BASROL: curr.value }
+            }
+            else if (curr.title.includes('Tema müziği bestecisi')) {
+                return { ...prev, TEMA_MUZIGI_BESTECI: curr.value }
+            }
+            else if (curr.title.includes('Besteci')) {
+                return { ...prev, BESTECI: curr.value }
+            }
+            else if (curr.title.includes('Ülke')) {
+                return { ...prev, ULKE: curr.value }
+            }
+            else if (curr.title.includes('Dili')) {
+                return { ...prev, DILI: curr.value }
+            }
+            else if (curr.title.includes('Sezon sayısı')) {
+                return { ...prev, SEZON_SAYISI: curr.value }
+            }
+            else if (curr.title.includes('Bölüm sayısı')) {
+                return { ...prev, BOLUM_SAYISI: curr.value }
+            }
+            else if (curr.title.includes('Yapımcı')) {
+                return { ...prev, YAPIMCI: curr.value }
+            }
+            else if (curr.title.includes('Mekân')) {
+                return { ...prev, MEKAN: curr.value }
+            }
+            else if (curr.title.includes('Görüntü yönetmeni')) {
+                return { ...prev, GORUNTU_YONETMEN: curr.value }
+            }
+            else if (curr.title.includes('Gösterim süresi')) {
+                return { ...prev, GOSTERIM_SURESI: curr.value }
+            }
+            else if (curr.title.includes('Yapım şirketi')) {
+                return { ...prev, YAPIM_SIRKETI: curr.value }
+            }
+            else if (curr.title.includes('Kanal')) {
+                return { ...prev, KANAL: curr.value }
+            }
+            else if (curr.title.includes('Görüntü formatı')) {
+                return { ...prev, GORUNTU_FORMATI: curr.value }
+            }
+            else if (curr.title.includes('Ses formatı')) {
+                return { ...prev, SES_FORMATI: curr.value }
+            }
+            else if (curr.title.includes('Yayın tarihi')) {
+                return { ...prev, YAYIN_TARIHI: curr.value }
+            }
+            else if (curr.title.includes('Durumu')) {
+                return { ...prev, DURUM: curr.value }
+            }
 
-        return { ...data, image }
+            return prev
+        }, {})
+
+        return {
+            ...data, POSTER: {
+                POSTER_IMG: image, POSTER_ORIENTATION: "portrait",
+                POSTER_QUALITY: 5
+            }
+        }
 
     })
 
-    return { bilgiler, dizi }
+    return { ...bilgiler, ...dizi }
 
 }
 
 
 const urls = ["https://tr.wikipedia.org/wiki/T%C3%BCrk_dizileri_listesi"]
 export { urls }
+
+
