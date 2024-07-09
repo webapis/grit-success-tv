136ec0cffb90046cc8e21b97bb3203b6f1649d00
Serdar Ashirov <webapis@users.noreply.github.com>
2024-07-09 08:48:34 +0500

    Update startv.js

--- sites/startv.js
+++ sites/startv.js
@@ -3,26 +3,31 @@ export default async function dizi({ page, enqueueLinks, request, log, addReques
 
 
     const data = await page.evaluate(() => {
-        const collection = Array.from(document.querySelectorAll(".poster-card")).map(m => {
+        const collection = Array.from(document.querySelectorAll(".grid .poster-card")).map(m => {
             // Check if elements exist before accessing properties
-            const title = m.querySelector("a img")?.getAttribute('title');
-            const detailHref = m.querySelector("a")?.href;
-            const img = m.querySelector("a img")?.getAttribute('src');
+            const TVSERIES_TITLE = m.querySelector("a img")?.getAttribute('title');
+            const WATCH_LINK = m.querySelector("a")?.href;
+            const DETAIL_LINK = m.querySelector("a")?.href;
+            const POSTER_IMG = m.querySelector("a img")?.getAttribute('src');
             return {
-                title,
-                detailHref,
-                img,
-                imgOrientation:"portrait",
-                imqQuatity:1
+                TVSERIES_TITLE,
+                WATCH_LINK,
+                DETAIL_LINK,
+
+                POSTER: {
+                    POSTER_IMG,
+                    POSTER_ORIENTATION: "portrait",
+                    POSTER_QUALITY: 1
+                },
             }
         });
-        return collection
+        return collection.filter(f=>!f.DETAIL_LINK.includes('/program/'))
     })
     for (let d of data) {
-        await addRequests([{ url: d.detailHref + '/oyuncular', label: 'oyuncular', userData: { dizi: d, initUrl: d.detailHref } }])
+        await addRequests([{ url: d.DETAIL_LINK + '/oyuncular', label: 'oyuncular', userData: { dizi: d } }])
     }
     debugger
-   // return data
+    // return data
 
 }
 
@@ -31,25 +36,38 @@ export async function oyuncular({ page, enqueueLinks, request, log, addRequests 
     debugger
     const { userData: { dizi } } = request
     debugger
-    const oyuncular = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".actor-card")).map(m => {
+    const ACTORS = await page.evaluate(() => {
+        function getElementByInnerText(text) {
+            const allElements = Array.from(document.body.getElementsByTagName('*'));
+            
+            return allElements.find(function(element) {
+              return element.innerText && element.innerText.trim() === text;
+            });
+          }
+
+          const SUMMARY = getElementByInnerText('Hikaye')?.nextElementSibling.innerText
+          const YONETMEN =getElementByInnerText('Yönetmen')?.nextElementSibling.innerText
+          const YAPIM_SIRKETI= getElementByInnerText('Yapım')?.nextElementSibling.innerText
+        const ACTORS= Array.from(document.querySelectorAll(".actor-card")).map(m => {
             // Check if elements exist before accessing properties
-            const actor = m.querySelector('.actor-card-content p')?.innerText;
-            const character = m.querySelector('h4')?.innerText
-            const img = document.querySelector('.actor-card-image img')?.getAttribute('src');
+            const ACTOR = m.querySelector('.actor-card-content p')?.innerText;
+            const CHARACTER = m.querySelector('h4')?.innerText
+            const ACTOR_IMAGE = document.querySelector('.actor-card-image img')?.getAttribute('src');
 
             return {
-                actor,
-                character,
-                img
+                ACTOR,
+                CHARACTER,
+                ACTOR_IMAGE
             }
         });
 
 
 
+return {ACTORS,SUMMARY,YONETMEN,YAPIM_SIRKETI}
+
     })
 
-    return { oyuncular, dizi }
+    return { ...ACTORS, ...dizi }
 
 
 }
@@ -59,5 +77,5 @@ const urls = ["https://www.startv.com.tr/dizi"]
 export { urls }
 
 // summary
-// yapım 
+// yapım
 //yönetmen
