5f107f39691bbe38b91b8b86ca85b927553b1c94
Serdar Ashirov <webapis@users.noreply.github.com>
2024-07-10 14:24:58 +0500

    Update trt1.js

--- sites/trt1.js
+++ sites/trt1.js
@@ -5,57 +5,60 @@ export default async function dizi({ page, enqueueLinks, request, log, addReques
     const data = await page.evaluate(() => {
         const collection = Array.from(document.querySelectorAll("ul li .thumbnail")).map(m => {
             // Check if elements exist before accessing properties
-            const title = m.querySelector(".category-title")?.innerText;
-            const detailHref = m.querySelector("a")?.href;
-            const img = m.querySelector("a img")?.getAttribute('src');
+            const TVSERIES_TITLE = m.querySelector(".category-title")?.innerText;
+            const WATCH_LINK = m.querySelector("a")?.href;
+            const DETAIL_LINK = m.querySelector("a")?.href;
+            const POSTER_IMG = m.querySelector("a img")?.getAttribute('src');
             return {
-                title,
-                detailHref,
-                img,
-                imgOrientation:"landscape",
-                imqQuatity:1
-                
+                TVSERIES_TITLE,
+                WATCH_LINK,
+                DETAIL_LINK,
+                POSTER: {
+                    POSTER_IMG,
+                    POSTER_ORIENTATION: "landscape",
+                    POSTER_QUALITY: 1
+                },
             }
         });
         return collection
     })
     for (let d of data) {
-        await addRequests([{ url: d.detailHref, label: 'oyuncular', userData: { dizi: d, initUrl: d.detailHref } }])
+        await addRequests([{ url: d.WATCH_LINK, label: 'oyuncular', userData: { dizi: d } }])
     }
     debugger
-   // return data
+    // return data
 
 }
 
 export async function oyuncular({ page, enqueueLinks, request, log, addRequests }) {
-    const currentUrl = await page.url()
+
     debugger
     const { userData: { dizi } } = request
     debugger
     const exist = await page.$$('.detail-stars')
     if (exist) {
-        const oyuncular = await page.evaluate(() => {
+        const ACTORS = await page.evaluate(() => {
             return Array.from(document.querySelectorAll(".oyuncu .thumbnail")).map(m => {
                 // Check if elements exist before accessing properties
-                const actor = m.querySelector('.stars-title-mid')?.innerText;
-                const character = m.querySelector('.stars-title')?.innerText
-                const img = m.querySelector('img')?.getAttribute('src');
-    
+                const ACTOR = m.querySelector('.stars-title-mid')?.innerText;
+                const CHARACTER = m.querySelector('.stars-title')?.innerText
+                const ACTOR_IMAGE = m.querySelector('img')?.getAttribute('src');
+
                 return {
-                    actor,
-                    character,
-                    img
+                    ACTOR,
+                    CHARACTER,
+                    ACTOR_IMAGE
                 }
             });
 
 
 
         })
-        return { oyuncular, dizi }
+        return { ACTORS, ...dizi }
     }
 
 
-    return { oyuncular: [], dizi }
+    return { ACTORS: [], ...dizi }
 
 
 }
