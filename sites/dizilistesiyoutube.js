091d88c531dd90387222650e07e3faebd03e3f0c
Serdar Ashirov <webapis@users.noreply.github.com>
2024-07-26 12:05:39 +0500

    Update dizilistesiyoutube.js

--- sites/dizilistesiyoutube.js
+++ sites/dizilistesiyoutube.js
@@ -7,11 +7,6 @@ debugger
         return Array.from(document.querySelectorAll('a.yt-simple-endpoint.style-scope.ytd-playlist-panel-video-renderer')).map((m) => {
             return {
                 TVSERIES_TITLE: m.querySelector('#byline-container span.style-scope.ytd-playlist-panel-video-renderer').textContent.trim(),
-                POSTER: {
-                    POSTER_IMG: m.querySelector('.yt-core-image').src,
-                    POSTER_ORIENTATION: "landscape",
-                    POSTER_QUALITY: 5
-                },
                 WATCH_LINK: m.href
             }
         })
@@ -23,5 +18,8 @@ debugger
 
 //m.querySelector('.yt-core-image').src
 
-const urls = ["https://www.youtube.com/watch?v=A0YQiurLom4&list=PLDqeHyMqCZ2xUIg82higJdctXBHjB6baQ","https://www.youtube.com/watch?v=Vs2vPMCHAr8&list=PLKWui-ds6V3NZD1JpHROKVKPyjj3eNs6x"]
+const urls = [
+    "https://www.youtube.com/watch?v=A0YQiurLom4&list=PLDqeHyMqCZ2xUIg82higJdctXBHjB6baQ",
+    "https://www.youtube.com/watch?v=Vs2vPMCHAr8&list=PLKWui-ds6V3NZD1JpHROKVKPyjj3eNs6x",
+    "https://www.youtube.com/watch?v=Vs2vPMCHAr8&list=PLKWui-ds6V3NZD1JpHROKVKPyjj3eNs6x"]
 export { urls }
