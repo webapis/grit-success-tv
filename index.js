9f990ac0216c5a245b01f843715a288709d06709
Serdar Ashirov <webapis@users.noreply.github.com>
2024-07-26 09:01:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ import testRouter, { testUrl } from './crawler-helper/testRouter.js';
 const site = process.env.site
 const local = process.env.local
 const test = process.env.test
+const HEADLESS = process.env.HEADLESS
 const windowsPath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 const linuxPath = "/usr/bin/google-chrome"
 const siteVar = await import(`./sites/${site}.js`)
 
-const urls = test==='true' ? testUrl: siteVar.urls
+const urls = test === 'true' ? testUrl : siteVar.urls
 const crawler = new PuppeteerCrawler({
   launchContext: { launchOptions: { executablePath: local ? windowsPath : linuxPath } },
-  requestHandler: test==='true'? testRouter: router,
+  requestHandler: test === 'true' ? testRouter : router,
   maxConcurrency: 1,
   preNavigationHooks,
-  navigationTimeoutSecs:120, headless:false,requestHandlerTimeoutSecs:600000
+  navigationTimeoutSecs: 120, headless: HEADLESS === 'false' ? false : true, requestHandlerTimeoutSecs: 600000
 
 });
 
