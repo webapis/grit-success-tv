7f986ba61d31d4a1e9fb4edc19b09c2d65ae0214
Serdar Ashirov <webapis@users.noreply.github.com>
2024-08-07 21:42:31 +0500

    Update patirti.js

--- sites/product/patirti.js
+++ sites/product/patirti.js
@@ -1,4 +1,5 @@
 
+import autoscroll from '../../src/autoscroll.js'
 export default async function first({ page, enqueueLinks, request, log, addRequests }) {
 
 
@@ -7,7 +8,7 @@ export default async function first({ page, enqueueLinks, request, log, addReque
         label: 'first',
     });
 
-
+await autoscroll(page,150)
     //pagination
    
     const data = await page.evaluate(() => {
@@ -15,7 +16,11 @@ export default async function first({ page, enqueueLinks, request, log, addReque
         const pageURL = document.URL
         const result = Array.from(document.querySelectorAll('.categoryitem')).map(m=>{
             const title = m.querySelector('.categoryname')?.innerText
-                const price = m.querySelector('.pricenew')?.innerText
+                const price = Array.from(document.querySelector('.pricenew')?.childNodes).reverse().map(node=>{
+            
+                      const price =node.nodeType === Node.TEXT_NODE? node.nodeValue.trim(): node.textContent
+                      return price
+                  })[0]
                 return{
                     title,
                     price
