3aa60716446295cc51eba919e12cbc4c86f74da0
Serdar Ashirov <webapis@users.noreply.github.com>
2024-08-08 16:33:55 +0500

    Create modalogy.js

---
+++ sites/product/modalogy.js
@@ -0,0 +1,51 @@
+
+export default async function first({ page, enqueueLinks, request, log, addRequests }) {
+
+    const url = await page.url()
+    await enqueueLinks({
+        selector: '.navigation a',
+        label: 'first',
+    });
+
+
+    //pagination
+
+    const productCount = await page.evaluate(() => parseInt(document.querySelector('.appliedFilter.FiltrelemeUrunAdet span')?.innerText.replace(/[^\d]/gi, '')))
+
+
+    const totalPages = Math.ceil(productCount / 24)
+
+
+
+
+    if (productCount > 0 && totalPages > 1) {
+
+        for (let i = 1; i <= totalPages; i++) {
+
+            await addRequests([{ url: `${url}?sayfa=${i}`, label: 'second' }])
+
+        }
+    }
+
+    const data = await page.evaluate(() => {
+        const pageTitle = document.title
+        const pageURL = document.URL
+        const result = Array.from(document.querySelectorAll('.productItem')).map(m => {
+            const title = m.querySelector('.productName')?.innerText
+            const price = m.querySelector('.discountPrice')?.innerText
+            return {
+                title,
+                price
+            }
+        })
+
+        return result.length > 0 ? result.map(m => { return { ...m, pageTitle, pageURL } }) : []
+    })
+
+    debugger
+    return data
+
+}
+
+const urls = ["https://www.modalogy.com.tr/"]
+export { urls }
