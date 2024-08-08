3d562dc7e22ee094465a6f786ab5f4f42003ff45
Serdar Ashirov <webapis@users.noreply.github.com>
2024-08-08 10:47:34 +0500

    Create lovemybody.js

---
+++ sites/product/lovemybody.js
@@ -0,0 +1,42 @@
+
+export default async function first({ page, enqueueLinks, request, log, addRequests }) {
+
+
+    await enqueueLinks({
+        selector: '.navigation a',
+        label: 'first',
+    });
+    await enqueueLinks({
+        selector: '.pagination a',
+        label: 'first',
+    });
+
+    //pagination
+
+    const data = await page.evaluate(() => {
+        const pageTitle = document.title
+        const pageURL = document.URL
+        const result = Array.from(document.querySelectorAll('.product-item-box')).map(m => {
+            const title = m.querySelector('.product-name')?.innerText
+            const discount = m.querySelector('.product-discounted-price')?.innerText
+
+            const price = m.querySelector('.product-list-price')?.innerText
+
+            return {
+                title,
+                price: discount ? discount : price,
+                oldPrice:price,
+                discount
+            }
+        })
+
+        return result.length > 0 ? result.map(m => { return { ...m, pageTitle, pageURL } }) : []
+    })
+
+    debugger
+    return data
+
+}
+
+const urls = ["https://www.lovemybody.com.tr/"]
+export { urls }
