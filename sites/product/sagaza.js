38166ea015f5ac8ea3578da1a003fc3e05921782
Serdar Ashirov <webapis@users.noreply.github.com>
2024-08-09 09:57:50 +0500

    Create sagaza.js

---
+++ sites/product/sagaza.js
@@ -0,0 +1,45 @@
+
+export default async function first({ page, enqueueLinks, request, log, addRequests }) {
+
+
+    await enqueueLinks({
+        selector: '.header__menu a',
+        label: 'first',
+    });
+
+  await enqueueLinks({
+        selector: '.pagination-custom a',
+        label: 'first',
+    });
+
+
+    //pagination
+
+ 
+
+
+    const data = await page.evaluate(() => {
+        const pageTitle = document.title
+        const pageURL = document.URL
+        const result = Array.from(document.querySelectorAll('.product-grid-item')).map(m=>{
+            const title = m.querySelector('.product__grid__title')?.innerText
+        const price = m.querySelector('.price')?.innerText
+        return {
+            title,
+            price
+            
+        }
+    })
+
+        return result.length > 0 ? result.map(m => { return { ...m, pageTitle, pageURL } }) : []
+    })
+
+    debugger
+    return data
+
+}
+
+
+
+const urls = ["https://sagaza.com/"]
+export { urls }
