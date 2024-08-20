0b633e26286b9a32ec892990c20dea1b9691633f
Serdar Ashyrov <tkm.house.new@gmail.com>
2024-08-20 07:08:34 +0000

    patirti

--- sites/product/patirti.js
+++ sites/product/patirti.js
@@ -1,40 +1,48 @@
+import autoscroll from "../../src/autoscroll.js";
+export default async function first({
+  page,
+  enqueueLinks,
+  request,
+  log,
+  addRequests,
+}) {
+  await autoscroll(page, 150);
+  //pagination
 
-import autoscroll from '../../src/autoscroll.js'
-export default async function first({ page, enqueueLinks, request, log, addRequests }) {
+  const data = await page.evaluate(() => {
+    const pageTitle = document.title;
+    const pageURL = document.URL;
+    const result = Array.from(document.querySelectorAll(".categoryitem")).map(
+      (m) => {
+        const title = m.querySelector(".categoryname")?.innerText;
+        const price = Array.from(
+          document.querySelector(".pricenew")?.childNodes
+        )
+          .reverse()
+          .map((node) => {
+            const price =
+              node.nodeType === Node.TEXT_NODE
+                ? node.nodeValue.trim()
+                : node.textContent;
+            return price;
+          })[0];
+        return {
+          title,
+          price,
+        };
+      }
+    );
 
+    return result.length > 0
+      ? result.map((m) => {
+          return { ...m, pageTitle, pageURL };
+        })
+      : [];
+  });
 
-  await enqueueLinks({
-        selector: '.navbar a',
-        label: 'first',
-    });
-
-await autoscroll(page,150)
-    //pagination
-   
-    const data = await page.evaluate(() => {
-        const pageTitle = document.title
-        const pageURL = document.URL
-        const result = Array.from(document.querySelectorAll('.categoryitem')).map(m=>{
-            const title = m.querySelector('.categoryname')?.innerText
-                const price = Array.from(document.querySelector('.pricenew')?.childNodes).reverse().map(node=>{
-            
-                      const price =node.nodeType === Node.TEXT_NODE? node.nodeValue.trim(): node.textContent
-                      return price
-                  })[0]
-                return{
-                    title,
-                    price
-                    
-                }
-            })
-
-        return result.length > 0 ? result.map(m => { return { ...m, pageTitle, pageURL } }) : []
-    })
-
-    debugger
-    return data
-
+  debugger;
+  return data;
 }
 
-const urls = ["https://www.patirti.com"]
-export { urls }
+const urls = ["https://www.patirti.com/buyuk-beden/"];
+export { urls };
