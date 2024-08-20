a10ed9e2d0d2fa1b8b4ee99d77c0181652364723
Serdar Ashyrov <tkm.house.new@gmail.com>
2024-08-20 10:51:03 +0000

    s

--- url-categories/buyuk_beden_elbise.js
+++ url-categories/buyuk_beden_elbise.js
@@ -8,7 +8,7 @@ const urls = [
   "https://www.lcw.com/elbise-t-149?koleksiyon=buyuk-beden",
   "https://www.modanisa.com/en/plus-size-dresses.htm",
   "https://www.buyukbedenstil.com/kategori/elbise",
-  // "https://www.siyezen.com/buyuk-beden-elbise",
+  "https://www.siyezen.com/buyuk-beden-elbise",
   // "https://www.alvinaonline.com/tr/buyuk-beden/yaz-kreasyonu/elbise/",
   // "https://www.miabutik.com.tr/kadin-buyuk-beden-elbise-modelleri",
   // "https://www.sementa.com/collections/kadin-buyuk-beden-elbise",
