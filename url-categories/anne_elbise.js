615398246a9d11ee2954863e1128f7799641f817
Serdar Ashirov <webapis@users.noreply.github.com>
2024-08-21 19:41:33 +0500

    d

---
+++ url-categories/anne_elbise.js
@@ -0,0 +1,7 @@
+const urls =[
+    "https://www.zeynepboutique.com/anne-elbise-1",
+    "https://www.ilmio.com/anne-elbiseleri",
+    "https://www.iconasline.com/anne-elbiseleri",
+    "https://www.sefamerve.com/t/anne-elbisesi/",
+    "https://modapinhan.com/buyuk-beden-anne-elbise-siyah"
+]
--- url-categories/buyuk_beden_elbise.js
+++ url-categories/buyuk_beden_elbise.js
@@ -13,6 +13,24 @@ const urls = [
   "https://www.miabutik.com.tr/kadin-buyuk-beden-elbise-modelleri",
   "https://www.sementa.com/collections/kadin-buyuk-beden-elbise",
   "https://www.modamerve.com/buyuk-beden-elbise",
+  //https://www.miraggiobuyukbeden.com/elbiseler,
+  //https://www.rmgbuyukbeden.com/kadin-buyuk-beden-elbise,
+  //https://www.egzbutik.com/buyuk-beden-elbise-78,
+  //https://www.luokk.co/buyuk-beden-elbise,
+  //https://modayiz.com/buyuk-beden-elbise/,
+  //https://www.pttavm.com/arama/buyuk-beden-elbise,
+  //https://www.bedrinxxl.com/elbise,
+  //https://www.eslinamoda.com/tr/elbise/,
+  //https://www.pazarium.com.tr/buyuk-beden-tesettur-elbise
+  //https://www.eslinamoda.com/tr/elbise/
+  //https://www.modaebva.com/buyuk-beden-tesettur-elbise
+  //https://www.selengiyim.com/buyuk-beden-elbise
+  //https://www.modailgi.com.tr/buyuk-beden-elbise
+  //https://ebsumu.com/buyuk-beden-kadin-elbise-modelleri
+  //https://www.silemoda.com/buyuk-beden-elbise
+  //https://lafaba.com/buyuk-beden-abiye-elbiseler
+  //https://www.modasena.com/tesettur-buyuk-beden
+  //https://modaberray.com/giyim-buyuk-beden-buyuk-beden-elbise
 ];
 
 export default urls;
