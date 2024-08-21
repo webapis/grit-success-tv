b06ea64a055ab78dc9d70da253af6e831634d248
Serdar Ashyrov <tkm.house.new@gmail.com>
2024-08-21 06:52:20 +0000

    bysaygi filter

--- sites/product/bysaygi.js
+++ sites/product/bysaygi.js
@@ -1,35 +1,43 @@
 import autoScroll from "../../src/autoscroll.js";
 
-export default async function first({ page, enqueueLinks, request, log, addRequests }) {
+export default async function first({
+  page,
+  enqueueLinks,
+  request,
+  log,
+  addRequests,
+}) {
+  // await enqueueLinks({
+  //     selector: '.nav-item a',
+  //     label: 'first',
+  // });
+  await autoScroll(page, 200);
 
+  const data = await page.evaluate(() => {
+    const pageTitle = document.title;
+    const pageURL = document.URL;
+    const result = Array.from(document.querySelectorAll(".ItemOrj")).map(
+      (m) => {
+        const title = m.querySelector(".productName").innerText;
+        const price = m.querySelector(".discountPrice").innerText;
+        return {
+          title,
+          price,
+        };
+      }
+    );
 
-    await enqueueLinks({
-        selector: '.nav-item a',
-        label: 'first',
-    });
-    await autoScroll(page, 150)
-
-    const data = await page.evaluate(() => {
-        const pageTitle = document.title
-        const pageURL = document.URL
-        const result = Array.from(document.querySelectorAll('.ItemOrj')).map((m) => {
-
-            const title = m.querySelector('.productName').innerText
-            const price = m.querySelector('.discountPrice').innerText
-            return {
-                title,
-                price
-            }
+    return result.length > 0
+      ? result.map((m) => {
+          return { ...m, pageTitle, pageURL };
         })
+      : [];
+  });
 
-        return result.length > 0 ? result.map(m => { return { ...m, pageTitle, pageURL } }) : []
-    })
-
-    debugger
-    return data
-
+  debugger;
+  return data;
 }
 
-const urls = ["https://www.bysaygi.com/tum-urunler"]
+const urls = ["https://www.bysaygi.com/buyuk-beden-58"];
 
-export { urls }
+export { urls };
--- url-categories/buyuk_beden_elbise.js
+++ url-categories/buyuk_beden_elbise.js
@@ -13,6 +13,7 @@ const urls = [
   "https://www.miabutik.com.tr/kadin-buyuk-beden-elbise-modelleri",
   "https://www.sementa.com/collections/kadin-buyuk-beden-elbise",
   "https://www.modamerve.com/buyuk-beden-elbise",
+  { url: "https://www.bysaygi.com/buyuk-beden-58", filter: ["Elbise"] },
 ];
 
 export default urls;
