06a51322675551cf8b911f85ad669101da81693f
Serdar Ashirov <webapis@users.noreply.github.com>
2024-08-22 15:21:42 +0500

    Create gelinlik.js

---
+++ url-categories/gelinlik.js
@@ -0,0 +1,23 @@
+const urls = [
+    "https://5fil.com/kategori/kadin/gelinlik/",
+    "https://www.abiyefon.com/gelinlik-modelleri",
+    "https://www.alfa-beta.com.tr/kategori/gelinlik-2",
+    "https://aysirashop.com/tr/n/gelinlik/",
+    "https://cengizakturk.com/collections/gelinlik-modelleri",
+    "https://www.dreamon.com.tr/kategori/gelinlik",
+    "https://dugun.com/gelinlik",
+    "https://www.galaway.co/23-gelin-elbiseleri",
+    "https://www.gardrops.com/kadin/gelinlik",
+    "https://www.kiyafetsepeti.com.tr/gelinlik?_gl=1*1bdl89i*_up*MQ..&gclid=Cj0KCQjwq_G1BhCSARIsACc7Nxqq2t6dja8RrwtQD27heS5aX6uCsGuJc5P9GM-dJdy48akAvWBWCiEaArL4EALw_wcB",
+    "https://medihacambaz.com/collections/shop-tum-gelinlikler",
+    "https://olegcassini.com.tr/gelinlik",
+    "https://www.omurgelinlik.com/gelinlik",
+    "https://www.rosegelinlik.com/urunlerimiz/",
+    "https://www.tarikediz.com/tr/gelinlikler/",
+    "https://tuvanam.com/collections/glam-you-bridal",
+    "https://www.vakko.com/wedding-c-305",
+
+  ];
+  
+  export default urls;
+  
