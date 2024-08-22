cd2ceb327e3b23d9806ffe27884b3318f19205ad
Serdar Ashirov <webapis@users.noreply.github.com>
2024-08-22 12:43:37 +0500

    Update pushToGit.js

--- src/pushToGit.js
+++ src/pushToGit.js
@@ -4,9 +4,9 @@ import { Dataset } from 'crawlee';
 
 dotenv.config({ silent: true });
 const gitFolder = process.env.gitFolder
-
+const URL_CATEGORIES = process.env.URL_CATEGORIES
 const site = process.env.site
 const dataset = await Dataset.open(gitFolder);
 const { items: data } = await dataset.getData()
 
-await uploadCollection({fileName: site, data,gitFolder})
+await uploadCollection({fileName: site||URL_CATEGORIES, data,gitFolder})
