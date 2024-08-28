fd38269f6389cd631ab51922fb98d5d3a4a1c765
Serdar Ashirov <webapis@users.noreply.github.com>
2024-08-28 10:16:38 +0500

    Update kadin_kazak.js

--- url-categories/kadin_kazak.js
+++ url-categories/kadin_kazak.js
@@ -1,3 +1,13 @@
-const urls =[]
+const urls =[
+    "https://www.adl.com.tr/kadin-kazak/",
+    "https://www.beymen.com/tr/kadin-giyim-triko-kazak-10048",
+    "https://www.bsl.com.tr/tr/triko-kazak/",
+    "https://www.defacto.com.tr/kadin-kazak",
+    "https://www.koton.com/kadin-kazak-suveter/",
+    "https://www.lcw.com/kadin-kazak-t-202",
+    "https://www.lovemybody.com.tr/kazak/",
+    "https://shop.mango.com/tr/tr/c/kadin/kazaklar-ve-h%C4%B1rkalar_f9a8c868",
+    "https://markapia.com/kadin-kazak-hirka"
+]
 
 export default urls
