a2276f4b67ee7ba9747477e2014f62b2fbf7db78
Serdar Ashirov <webapis@users.noreply.github.com>
2024-08-29 15:47:21 +0500

    Update kadin_kazak.js

--- url-categories/kadin_kazak.js
+++ url-categories/kadin_kazak.js
@@ -1,18 +1,18 @@
 const urls = [
-   // "https://www.adl.com.tr/kadin-kazak/",
+    "https://www.adl.com.tr/kadin-kazak/",
     "https://www.beymen.com/tr/kadin-giyim-triko-kazak-10048",
-    // { url: "https://www.bsl.com.tr/tr/triko-kazak/", filter: ["kazak"] },
-    // "https://www.defacto.com.tr/kadin-kazak",
+    { url: "https://www.bsl.com.tr/tr/triko-kazak/", filter: ["kazak"] },
+    "https://www.defacto.com.tr/kadin-kazak",
     { url: "https://www.koton.com/kadin-kazak-suveter/", filter: ["kazak"] },
-    // "https://www.lcw.com/kadin-kazak-t-202",
-    // "https://www.lovemybody.com.tr/kazak/",
-    // { url: "https://shop.mango.com/tr/tr/c/kadin/kazaklar-ve-h%C4%B1rkalar_f9a8c868", filter: ["kazak"] },
-    // { url: "https://markapia.com/kadin-kazak-hirka", filter: ["kazak"] },
-    // "https://www.network.com.tr/kadin-kazak-cesitleri-1325",
-    // { url: "https://www.nocturne.com.tr/", filter: ["kazak"] },
-    // "https://www.oxxo.com.tr/tr/giyim/trikolar/kazaklar/",
-    // "https://www.patirti.com/kadin-triko-kazak/",
-    // "https://www.sementa.com/collections/kadin-kazak"
+    "https://www.lcw.com/kadin-kazak-t-202",
+    "https://www.lovemybody.com.tr/kazak/",
+    { url: "https://shop.mango.com/tr/tr/c/kadin/kazaklar-ve-h%C4%B1rkalar_f9a8c868", filter: ["kazak"] },
+    { url: "https://markapia.com/kadin-kazak-hirka", filter: ["kazak"] },
+    "https://www.network.com.tr/kadin-kazak-cesitleri-1325",
+    { url: "https://www.nocturne.com.tr/", filter: ["kazak"] },
+    "https://www.oxxo.com.tr/tr/giyim/trikolar/kazaklar/",
+    "https://www.patirti.com/kadin-triko-kazak/",
+    "https://www.sementa.com/collections/kadin-kazak"
 ]
 
 export default urls
