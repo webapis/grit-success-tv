48c09f4bc0405ff19dcad3a5e96595ad4b9f7d3c
Serdar Ashirov <webapis@users.noreply.github.com>
2024-08-29 15:24:34 +0500

    c

--- sites/product/beymen.js
+++ sites/product/beymen.js
@@ -9,7 +9,7 @@ export default async function first({ page, enqueueLinks, request, log, addReque
     // });
 
 
-    const productCount = await page.evaluate(() => parseInt(document.querySelector('.o-productList__top--breadcrumbCount')?.innerText.replace(/[^\d]/gi, '') ))
+    const productCount = await page.evaluate(() => parseInt(document.querySelector('.o-productList__top--breadcrumbCount')?.innerText?.replace(/[^\d]/gi, '') ))
     const totalPages = Math.ceil(productCount / 48)
     await addRequests([{ url, label: 'second' }])
     if (productCount > 0 && totalPages > 1) {
