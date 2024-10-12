b559e07b65059603bf3f03ac4898f847516c2bc0
Serdar Ashirov <webapis@users.noreply.github.com>
2024-10-12 12:59:36 +0500

    Update lovemybody.js

--- sites/sponsor-product/lovemybody.js
+++ sites/sponsor-product/lovemybody.js
@@ -1,11 +1,11 @@
 
 export default async function first({ page, enqueueLinks, request, log, addRequests }) {
+debugger
 
-
-    // await enqueueLinks({
-    //     selector: '.navigation a',
-    //     label: 'first',
-    // });
+    await enqueueLinks({
+        selector: '.navigation a',
+        label: 'first',
+    });
     await enqueueLinks({
         selector: '.pagination a',
         label: 'first',
@@ -18,20 +18,26 @@ export default async function first({ page, enqueueLinks, request, log, addReque
         const pageURL = document.URL
         const result = Array.from(document.querySelectorAll('.product-item-box')).map(m => {
             const title = m.querySelector('.product-name')?.innerText
-            const discount = m.querySelector('.product-discounted-price')?.innerText
-
-            const price = m.querySelector('.product-list-price')?.innerText
+            const sepeteIndirim = document.querySelector('.product-item__campaign')? Array.from(document.querySelector('.product-item__campaign')?.querySelectorAll('span'))?.reverse()[0]?.innerText:undefined
+            const productDiscountPrice = document.querySelector('.product-discounted-price')?.innerText
+            const productSalePrice = document.querySelector('.product-sale-price')?.innerText
             const img = m.querySelector("[src]")?.src
-            const link = m.querySelector("a").href
+            const link = m.querySelector("a.product-name").href
+            try {
+                return {
+                    title,
+                    price: sepeteIndirim ? sepeteIndirim : (productDiscountPrice ? productDiscountPrice : productSalePrice),
+                    img,
+                    link,
+                    sepeteIndirim,
+                    productDiscountPrice,
+                    productSalePrice
 
-            return {
-                title,
-                price: discount ? discount : price,
-                oldPrice: price,
-                discount,
-                img,
-                link
+                }
+            } catch (error) {
+                return { error, content: m.innerHTML }
             }
+
         })
 
         return result.length > 0 ? result.map(m => { return { ...m, pageTitle, pageURL } }) : []
