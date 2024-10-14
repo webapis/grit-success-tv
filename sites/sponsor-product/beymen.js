f426574966a87aa8d6a52f7fd46e2ca40dbc74ae
Serdar Ashirov <webapis@users.noreply.github.com>
2024-10-14 14:26:20 +0500

    Update beymen.js

--- sites/sponsor-product/beymen.js
+++ sites/sponsor-product/beymen.js
@@ -3,10 +3,10 @@ export default async function first({ page, enqueueLinks, request, log, addReque
 
     const url = await page.url()
 
-    // await enqueueLinks({
-    //     selector: 'a.m-siteMap__link',
-    //     label: 'first',
-    // });
+    await enqueueLinks({
+        selector: 'a.m-siteMap__link',
+        label: 'first',
+    });
 
 
     // const productCount = await page.evaluate(() => parseInt(document.querySelector('.o-productList__top--breadcrumbCount')?.innerText?.replace(/[^\d]/gi, '')))
@@ -20,40 +20,50 @@ export default async function first({ page, enqueueLinks, request, log, addReque
 
     //     }
     // }
+    const productItemsCount = await page.locator('.o-productList').count();
+    if (productItemsCount > 0) {
+        const data = await page.evaluate(() => {
+            const pageTitle = document.title
+            const pageURL = document.URL
+            const result = Array.from(document.querySelectorAll('.o-productList__itemWrapper')).map(document => {
+                try {
+                    const title = document.querySelector('.m-productCard__desc')?.innerText
+                    const price = document.querySelector('.m-productCard__newPrice')?.innerText
+                    const lastPrice = document.querySelector('.m-productCard__lastPrice')?.innerText
+                    const img = document.querySelector('[data-src]')?.getAttribute('data-src')
+                    const link = document.querySelector('.m-productCard__photo a').href
+                    return {
+                        title,
+                        price: lastPrice ? lastPrice : price,
+                        img,
+                        link,
+                        pageTitle, pageURL
 
+                    }
+                } catch (error) {
+                    return { error, content: document.innerHTML, pageURL }
+                }
 
+            })
 
-}
-
-export async function second({ page, enqueueLinks, request, log, addRequests }) {
-
-    const data = await page.evaluate(() => {
-        const pageTitle = document.title
-        const pageURL = document.URL
-        const result = Array.from(document.querySelectorAll('.o-productList__itemWrapper')).map(m => {
-            const title = m.querySelector('.m-productCard__desc')?.innerText
-            const price = m.querySelector('.m-productCard__newPrice')?.innerText
-            const lastPrice = m.querySelector('.m-productCard__lastPrice')?.innerText
-            const img = m.querySelector('[data-src]')?.getAttribute('data-src')
-            const link = m.querySelector('.m-productCard__photo a').href
-            return {
-                title,
-                price: lastPrice ? lastPrice : price,
-                img,
-                link
-
-            }
+            return result
         })
 
-        return result.length > 0 ? result.map(m => { return { ...m, pageTitle, pageURL } }) : []
-    })
 
 
 
+        debugger
+        return data.map(m => { return { ...m, price: m.price.replace("Ek İndirimle\n", '') } })
+    } else {
 
-    debugger
-    return data.map(m => {return {...m, price: m.price.replace("Ek İndirimle\n", '')}})
+        return []
+    }
+
+
+
 }
 
-const urls = ["https://www.beymen.com/tr/kadin-giyim-triko-kazak-10048"]
+
+
+const urls = ["https://www.beymen.com/home/sitemap"]
 export { urls }
