807967b2f942a6f4b6da394da4323859d69fdcfd
Serdar Ashirov <webapis@users.noreply.github.com>
2024-10-14 12:16:18 +0500

    Update oxxo.js

--- sites/sponsor-product/oxxo.js
+++ sites/sponsor-product/oxxo.js
@@ -4,61 +4,53 @@ export default async function first({ page, enqueueLinks, request, log, addReque
 
     const url = await page.url()
 
-    const urls = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('#MainMenu a')).map(m => m.href).filter((f) => !f.includes('undefined') && !f.includes('javascript'))
-    })
-    for (let u of urls) {
-
-        await addRequests([{ url: `${u}?p=100`, label: 'second' }])
-    }
-
-    // const dataLayer = await page.evaluate(() => {
-
-    //     return dataLayer
-    // })
-    // const category = dataLayer.find((f) => f.event === 'ee_productImpression')
+    await enqueueLinks({
+        selector: '.main-menu a',
+        label: 'first',
+    });
 
 
-    // if (category) {
+    const productItemsCount = await page.locator('.Products').count();
+    if (productItemsCount > 0) {
+        const data = await page.evaluate(() => {
+            const pageTitle = document.title
+            const pageURL = document.URL
+            const result = Array.from(document.querySelectorAll('.Prd')).map(document => {
+                try {
+                    const title = document.querySelector('.PName')?.innerText
+                    const price = document.querySelector('.PPrice')?.innerText
+                    const img = document.querySelector('[data-src]')?.getAttribute('data-src')
+                    const img1 = document.querySelector('.PImage')?.src
+                    const link = document.querySelector("a[data-product]").href
+                    return {
+                        title,
+                        price,
+                        img: img || img1,
+                        link,
+                        pageTitle,
+                        pageURL
+                    }
+                } catch (error) {
+                    return { error, content: document.innerHTML, pageURL }
+                }
 
-    //     const pageUrl = url + `?p=100`
-    //     debugger
-    //     await addRequests([{ url: pageUrl, label: 'second' }])
+            })
 
-    // } else {
-
-    //     return []
-    // }
-
-
-}
-
-
-export async function second({ page, enqueueLinks, request, log, addRequests }) {
-
-    const data = await page.evaluate(() => {
-        const pageTitle = document.title
-        const pageURL = document.URL
-        const result = Array.from(document.querySelectorAll('.Prd')).map(m => {
-            const title = m.querySelector('.PName')?.innerText
-            const price = m.querySelector('.PPrice')?.innerText
-             const img =m.querySelector('[data-src]')?.getAttribute('data-src')
-             const img1 =m.querySelector('.PImage')?.src
-             const link =m.querySelector("a[data-product]").href
-            return {
-                title,
-                price,
-                img:img|| img1,
-                link
-            }
+            return result
         })
 
-        return result.length > 0 ? result.map(m => { return { ...m, pageTitle, pageURL } }) : []
-    })
+        return data
+    } else {
+        console.log('not product page:', url)
+        return []
+    }
 
-    return data
+
+
 
 }
+
+
 
 const urls = ["https://www.oxxo.com.tr/"]
 export { urls }
