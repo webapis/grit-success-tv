4fee2322957eda50e1897eec5eb7f9d39b82d4a2
Serdar Ashirov <webapis@users.noreply.github.com>
2024-10-14 13:13:26 +0500

    Update yargici.js

--- sites/sponsor-product/yargici.js
+++ sites/sponsor-product/yargici.js
@@ -1,33 +1,49 @@
 
 export default async function first({ page, enqueueLinks, request, log, addRequests }) {
 
+    await enqueueLinks({
+        selector: '.first-catogry a',
+        label: 'first',
+    });
 
- 
     //pagination
-   
-    const data = await page.evaluate(() => {
-        const pageTitle = document.title
-        const pageURL = document.URL
-        const result = Array.from(document.querySelectorAll('.prod-card')).map(m=>{
-            const title =m.querySelector('.productbox-name')?.innerText
-             const price =m.querySelector('.product-box-price')?.innerText
-            const img =m.querySelector("[data-src]").getAttribute('data-src')
-            const link =m.querySelector('a').href
-             return {
-                 title,
-                 price,
-                 img,
-                 link
-             }
-         })
+    const productItemsCount = await page.locator('.product-list-container').count();
+    if (productItemsCount > 0) {
+        const data = await page.evaluate(() => {
+            const pageTitle = document.title
+            const pageURL = document.URL
+            const result = Array.from(document.querySelectorAll('.prod-card')).map(document => {
+                try {
+                    const title = document.querySelector('.productbox-name')?.innerText
+                    const price = document.querySelector('.product-box-price')?.innerText
+                    const img = document.querySelector("[data-src]").getAttribute('data-src')
+                    const link = document.querySelector('a').href
+                    return {
+                        title,
+                        price,
+                        img,
+                        link,
+                        pageTitle,
+                        pageURL
+                    }
+                } catch (error) {
+                    return { error, content: document.innerHTML, pageURL }
+                }
 
-        return result.length > 0 ? result.map(m => { return { ...m, pageTitle, pageURL } }) : []
-    })
+            })
 
-    debugger
-    return data
+            return result
+        })
+
+        debugger
+        return data
+    } else {
+
+        return []
+    }
+
 
 }
 
-const urls = ["https://www.yargici.com/yeni-sezon-elbise"]
+const urls = ["https://www.yargici.com"]
 export { urls }
