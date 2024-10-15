886f5aa4c2b9bd282eb5034f3a382345ecc29b03
Serdar Ashirov <webapis@users.noreply.github.com>
2024-10-15 08:56:34 +0500

    Create muun.js

---
+++ sites/sponsor-product/muun.js
@@ -0,0 +1,57 @@
+
+
+export default async function first({ page, enqueueLinks, request, log, addRequests }) {
+
+    const url = await page.url()
+    await enqueueLinks({
+        selector: '.nav-item a',
+        label: 'first',
+    });
+
+
+
+    const productItemsCount = await page.locator('.ProductList').count();
+
+    if (productItemsCount > 0) {
+        const data = await page.evaluate(() => {
+
+            const pageTitle = document.title
+            const pageURL = document.URL
+            const result = Array.from(document.querySelectorAll('.productItem')).map(document => {
+                try {
+                    const title = document.querySelector('.detailLink.detailUrl').getAttribute('title')
+                    const price = document.querySelector('.discountPriceSpan').innerText
+                    const img = document.querySelector('.productImage img[data-original]').getAttribute('data-original')
+                    const link = document.querySelector('.detailLink.detailUrl').href
+
+                    return {
+                        title,
+                        price,
+                        img,
+                        link,
+                        pageTitle,
+                        pageURL
+                    }
+                } catch (error) {
+                    
+                    return { error, message: error.message, pageURL, content: document.innerHTML }
+                }
+
+            })
+
+            return result
+
+        })
+
+        debugger
+        return data
+    } else {
+
+        return []
+    }
+}
+
+
+
+const urls = ["https://www.muun.com.tr/"]
+export { urls }
