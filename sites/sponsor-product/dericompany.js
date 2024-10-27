9633eb11288c6eb21e86d32d7167f872105a5eb3
Serdar Ashirov <webapis@users.noreply.github.com>
2024-10-27 14:41:16 +0500

    dericompany

---
+++ sites/sponsor-product/dericompany.js
@@ -0,0 +1,60 @@
+
+import autscroll from '../../src/autoscroll.js'
+export default async function first({ page, enqueueLinks, request, log, addRequests }) {
+
+    const url = await page.url()
+    
+    await enqueueLinks({
+        selector: '.nav-links a',
+        label: 'first',
+    });
+    await enqueueLinks({
+        selector: '.paginations a',
+        label: 'first',
+    });
+    await autscroll(page, 200)
+
+    const productItemsCount = await page.locator('.category__list__main').count();
+    if (productItemsCount > 0) {
+        const data = await page.evaluate(() => {
+            const pageTitle = document.title
+            const pageURL = document.URL
+            const content= document.innerHTML
+            try {
+ 
+                const result = Array.from(document.querySelectorAll('a.grid')).map(document => {
+
+                    const title = document.querySelector(".product-name")?.innerText
+
+                    const price = document.querySelector(".price-main")?.innerText
+
+                    const img = document.querySelector('src')?.getAttribute('src') //|| document.querySelector('[srcset]')?.getAttribute('srcset') || document.innerHTML//.split(',')[8].trim().split(' ')[0]
+                    //const img1 = document.querySelector('[src]')?.src
+                    const link = document.href
+                    return {
+                        title,
+                        link,
+                        price,
+                        img,
+                        pageTitle,
+                        pageURL
+                    }
+                })
+
+                return result
+            } catch (error) {
+                return { error, message: error.message, content ,pageURL }
+            }
+        })
+        debugger
+        return data
+    } else {
+        return []
+    }
+
+}
+
+
+
+const urls = ["https://dericompany.com.tr/kadin-deri-mont", "https://dericompany.com.tr/erkek-deri-ceket", "https://dericompany.com.tr/deri-kemer-deri-cuzdan"]
+export { urls }
