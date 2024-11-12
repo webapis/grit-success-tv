93c2eb50a0c8b937ab0672abe1a190cc35cb05f4
Serdar Ashirov <webapis@users.noreply.github.com>
2024-11-12 10:11:05 +0500

    Update defacto.js

--- sites/sponsor-product/defacto.js
+++ sites/sponsor-product/defacto.js
@@ -1,14 +1,46 @@
 
 export default async function first({ page, enqueueLinks, request, log, addRequests }) {
+    debugger
 
     const url = await page.url()
+    let urls = []
+    debugger
+    // const varOne = await page.$$('.story__item a')
+    // debugger
 
-    await enqueueLinks({
-        selector: '.page-body-content-sitemap a',
-        label: 'first',
-    });
+    // if (varOne) {
+    //     urls = await page.evaluate(() => {
+    //         return Array.from(document.querySelectorAll('.story__item a')).map(m => m.href).filter(f => f)
+    //     })
+
+    // }
+    const varTwo = await page.$$('.product-card__image')
+    if (varTwo) {
+        debugger
+        urls = await page.evaluate(() => {
+            return Array.from(document.querySelectorAll('.product-card__image a')).map(m => m.href).filter(f => f)
+        })
+    }
+    debugger
 
 
+    if (urls.length === 0) {
+        throw `urls.length===0 :${url}:`
+    }
+    console.log('aggregation urls.length', urls.length)
+    console.log('aggregation urls', urls)
+
+    for (let u of urls) {
+
+        await addRequests([{ url: u, label: 'second' }])
+    }
+
+
+
+
+}
+
+export async function second({ page, enqueueLinks, request, log, addRequests }) {
     const productItemsCount = await page.locator('[data-documents]').count();
     if (productItemsCount > 0) {
         const data = await page.evaluate(() => {
@@ -43,12 +75,23 @@ export default async function first({ page, enqueueLinks, request, log, addReque
     } else {
         return []
     }
-
 }
 
-export async function second({ page, enqueueLinks, request, log, addRequests }) {
-
-}
-
-const urls = ["https://www.defacto.com.tr/statik/sitemap"]
+const urls = ["https://www.defacto.com.tr/kadin-giyim",
+    "https://www.defacto.com.tr/kadin-ayakkabi",
+    "https://www.defacto.com.tr/kadin-aksesuar",
+    "https://www.defacto.com.tr/erkek-giyim",
+    "https://www.defacto.com.tr/erkek-ayakkabi",
+    "https://www.defacto.com.tr/erkek-aksesuar",
+    "https://www.defacto.com.tr/kiz-cocuk-genc-kiz-giyim",
+    "https://www.defacto.com.tr/kiz-cocuk-genc-kiz-aksesuar",
+    "https://www.defacto.com.tr/kiz-cocuk-genc-kiz-ayakkabi",
+    "https://www.defacto.com.tr/erkek-cocuk-genc-erkek-giyim",
+    "https://www.defacto.com.tr/erkek-cocuk-genc-erkek-aksesuar",
+    "https://www.defacto.com.tr/erkek-cocuk-genc-erkek-ayakkabi",
+    "https://www.defacto.com.tr/kiz-bebek-giyim",
+    "https://www.defacto.com.tr/kiz-bebek-aksesuar",
+    "https://www.defacto.com.tr/kiz-bebek-ayakkabi",
+    "https://www.defacto.com.tr/erkek-bebek-giyim",
+    "https://www.defacto.com.tr/yenidogan"]
 export { urls }
