76c83ac65a99dfd8a9b9129de8709ae863f188e4
Serdar Ashirov <webapis@users.noreply.github.com>
2024-11-18 15:47:20 +0500

    Update burberry.js

--- sites/sponsor-product-11/burberry.js
+++ sites/sponsor-product-11/burberry.js
@@ -1,9 +1,11 @@
 
+import autoscroll from "../../src/autoscroll.js"
+
 export default async function first({ page, enqueueLinks, request, log, addRequests }) {
 
     const url = await page.url()
 
-    //await autoScroll(page, 200);
+
     debugger
 
     //await page.waitForSelector('.navigation__item')
@@ -22,25 +24,27 @@ export default async function first({ page, enqueueLinks, request, log, addReque
 export async function second({ page }) {
     const url = await page.url()
 
-    const productItemsCount = await page.locator('.product-grid-item').count();
+    const productItemsCount = await page.locator('.product-listing-shelf__product-card').count();
     if (productItemsCount > 0) {
+
         const data = await page.evaluate(() => {
             const pageTitle = document.title
             const pageURL = document.URL
-            const result = Array.from(document.querySelectorAll('.product-grid-item')).map(document => {
+            const result = Array.from(document.querySelectorAll('.product-listing-shelf__product-card')).map(document => {
                 try {
-                    const title = document.querySelector('.product-tile-body__link').innerText
+                    const title = document.querySelector('.product-card-v2-title').innerText
                     //lazyloaded        
                     //swiper-lazy
-                    const img1 = document.querySelector('.product-tile-image__picture  img.lazyloaded')?.scr
-                    const img2 = document.querySelector('.product-tile-image__picture  img.swiper-lazy')?.dataset.src
+                    const img1 = document.querySelector('.media-carousel-container__media.product-card-v2-carousel-container__media__source').src
+                    const img2 = document.querySelector('picture source ')?.srcset.split(' ')[0]
+                    const price = document.querySelector('.product-card-v2-price__current')?.innerText
                     //  const img = document.querySelector('.product-tile-image__picture source').dataset.srcset
 
-                    const link = document.querySelector('.product-tile-body__link').href
+                    const link = document.querySelector('a').href
                     return {
                         title,
-                        price: 0,
-                        img: img1 || img2,
+                        price,
+                        img: img1, //|| img2 ,
                         link,
                         pageTitle, pageURL
 
