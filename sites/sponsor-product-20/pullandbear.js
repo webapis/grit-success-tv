9e571d10471c9235e78805a4e7501c9f8582414e
Serdar Ashirov <webapis@users.noreply.github.com>
2024-11-19 10:45:07 +0500

    Update pullandbear.js

--- sites/sponsor-product-20/pullandbear.js
+++ sites/sponsor-product-20/pullandbear.js
@@ -22,25 +22,21 @@ export default async function first({ page, enqueueLinks, request, log, addReque
 export async function second({ page }) {
     const url = await page.url()
 
-    const productItemsCount = await page.locator('.product-grid-item').count();
+    const productItemsCount = await page.locator('legacy-product').count();
     if (productItemsCount > 0) {
         const data = await page.evaluate(() => {
             const pageTitle = document.title
             const pageURL = document.URL
-            const result = Array.from(document.querySelectorAll('.product-grid-item')).map(document => {
+            const result = Array.from(document.querySelectorAll('legacy-product')).map(document => {
                 try {
-                    const title = document.querySelector('.product-tile-body__link').innerText
-                    //lazyloaded        
-                    //swiper-lazy
-                    const img1 = document.querySelector('.product-tile-image__picture  img.lazyloaded')?.scr
-                    const img2 = document.querySelector('.product-tile-image__picture  img.swiper-lazy')?.dataset.src
-                    //  const img = document.querySelector('.product-tile-image__picture source').dataset.srcset
-
-                    const link = document.querySelector('.product-tile-body__link').href
+                    const title = document.querySelector('.name span').innerText
+                    const img1 = document.querySelector('.image-responsive').src
+                    const price =document.querySelector('.product-price--price')?.innerText
+                    const link = document.querySelector('.carousel-item-container').href
                     return {
                         title,
-                        price: 0,
-                        img: img1 || img2,
+                        price,
+                        img: img1,
                         link,
                         pageTitle, pageURL
 
