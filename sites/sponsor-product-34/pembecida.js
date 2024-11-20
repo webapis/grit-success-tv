2e413637ce02c4d5e9dfaf232b6737d73903e2a6
Serdar Ashirov <webapis@users.noreply.github.com>
2024-11-20 08:39:49 +0500

    lovemetoo

---
+++ sites/sponsor-product-34/pembecida.js
@@ -0,0 +1,73 @@
+
+export default async function first({ page, enqueueLinks, request, log, addRequests }) {
+
+    const url = await page.url()
+
+    //await autoScroll(page, 200);
+    debugger
+
+    //await page.waitForSelector('.navigation__item')
+
+    const urls = await page.evaluate(() => {
+
+        return Array.from(document.querySelectorAll('a')).map(m => m.href)
+    })
+    console.log('aggregation urls', urls)
+    for (let u of urls) {
+        await addRequests([{ url: u, label: 'second' }])
+    }
+
+}
+
+export async function second({ page }) {
+    const url = await page.url()
+
+    const productItemsCount = await page.locator('.product-grid-item').count();
+    if (productItemsCount > 0) {
+        const data = await page.evaluate(() => {
+            const pageTitle = document.title
+            const pageURL = document.URL
+            const result = Array.from(document.querySelectorAll('.product-grid-item')).map(document => {
+                try {
+                    const title = document.querySelector('.product-tile-body__link').innerText
+                    //lazyloaded        
+                    //swiper-lazy
+                    const img1 = document.querySelector('.product-tile-image__picture  img.lazyloaded')?.scr
+                    const img2 = document.querySelector('.product-tile-image__picture  img.swiper-lazy')?.dataset.src
+                    //  const img = document.querySelector('.product-tile-image__picture source').dataset.srcset
+
+                    const link = document.querySelector('.product-tile-body__link').href
+                    return {
+                        title,
+                        price: 0,
+                        img: img1 || img2,
+                        link,
+                        pageTitle, pageURL
+
+                    }
+                } catch (error) {
+                    return { error, message: error.message, content: document.innerHTML, pageURL }
+                }
+
+            })
+
+            return result.filter(f => f.img)
+        })
+
+
+        console.log('data.length', data.length)
+
+        debugger
+        return data
+    } else {
+        console.log('not a product page', url)
+        return []
+    }
+
+}
+
+const urls = [
+     "https://www.pembecida.com/",
+ 
+]
+export { urls }
--- sites/sponsor-product/lovemetoo.js
+++ sites/sponsor-product/lovemetoo.js
@@ -30,45 +30,53 @@ export default async function first({ page, enqueueLinks, request, log, addReque
 export async function second({ page, enqueueLinks, request, log, addRequests }) {
     const url = await page.url()
 
-    const productItemsCount = await page.locator('.product-list').count();
-    if (productItemsCount > 0) {
 
-
-        const data = await page.evaluate(() => {
-            const pageTitle = document.title
-            const pageURL = document.URL
-            const result = Array.from(document.querySelectorAll('.product-block')).map(document => {
-
-                try {
-
-                    const title = document.querySelector('.product-block__title').innerText
-
-                    const price = document.querySelector('.money.conversion-bear-money').innerText
-                    const img = 'https:' + document.querySelector('[data-lazy-bgset-src]').getAttribute('data-lazy-bgset-src')
-                    const link = document.querySelector('.product-link').href
-                    return {
-                        title,
-                        price,
-                        img,
-                        link,
-                        pageTitle,
-                        pageURL
-
+    try {
+        await page.waitForSelector('.product-block')
+        const productItemsCount = await page.locator('.product-block').count();
+        if (productItemsCount > 0) {
+    
+    
+            const data = await page.evaluate(() => {
+                const pageTitle = document.title
+                const pageURL = document.URL
+                const result = Array.from(document.querySelectorAll('.product-block')).map(document => {
+    
+                    try {
+    
+                        const title = document.querySelector('.product-block__title').innerText
+    
+                        const price = document.querySelector('.money.conversion-bear-money').innerText
+                        const img = 'https:' + document.querySelector('[data-lazy-bgset-src]').getAttribute('data-lazy-bgset-src')
+                        const link = document.querySelector('.product-link').href
+                        return {
+                            title,
+                            price,
+                            img,
+                            link,
+                            pageTitle,
+                            pageURL
+    
+                        }
+                    } catch (error) {
+                        return { error, meggage: error.message, content: document.innerHTML, pageURL }
                     }
-                } catch (error) {
-                    return { error, meggage: error.message, content: document.innerHTML, pageURL }
-                }
-
+    
+                })
+    
+                return result
             })
-
-            return result
-        })
-
-        return data
-    } else {
+    
+            return data
+        } else {
+            console.log('not product page:', url)
+            return []
+        }
+    } catch (error) {
         console.log('not product page:', url)
         return []
     }
+   
 }
 
 const urls = ["https://lovemetoo.com.tr/"]
