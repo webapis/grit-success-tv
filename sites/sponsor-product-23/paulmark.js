92d7283a8166142b50219426e43bfd8cbc5b8041
Serdar Ashirov <webapis@users.noreply.github.com>
2024-11-22 17:22:24 +0500

    b

--- sites/sponsor-product-23/paulmark.js
+++ sites/sponsor-product-23/paulmark.js
@@ -1,4 +1,5 @@
 
+import autoScroll from "../../src/autoscroll.js"
 export default async function first({ page, enqueueLinks, request, log, addRequests }) {
 
     const url = await page.url()
@@ -27,20 +28,22 @@ const getProductCount = async (page) => {
     } catch (error) {
         return 0;
     }
-};
+}
+
 export async function second({ page }) {
     const url = await page.url()
     const productItemsCount =await getProductCount(page)
     // const productItemsCount = await page.locator('.productItemLayout.ViewProduct').count();
     if (productItemsCount > 0) {
+        await autoScroll(page,150)
         const data = await page.evaluate(() => {
             const pageTitle = document.title
             const pageURL = document.URL
             const result = Array.from(document.querySelectorAll('.productItemLayout.ViewProduct')).map(document => {
                 try {
-                    const title = document.querySelector('.product-name').innerText
+                    const title =document.querySelector('img').alt// document.querySelector('.product-name').innerText
                     const price = document.querySelector('.second-price.discountActivePrice')?.innerText || document.querySelector('.second-price')?.innerText
-                    const img1 = document.querySelector('img.lazy').getAttribute('data-src')
+                    const img1 = document.querySelector('img').getAttribute('data-src')
 
 
                     const link = document.querySelector('a.item-img').href
@@ -58,7 +61,7 @@ export async function second({ page }) {
 
             })
 
-            return result.filter(f => f.img)
+            return result//.filter(f => f.img)
         })
 
 
@@ -71,7 +74,8 @@ export async function second({ page }) {
         return []
     }
 
-}
+} 
+
 
 const urls = [
     "https://www.paulmark.com.tr/",
